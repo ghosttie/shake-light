ce777dfc2a61c56d36a1d6fb7aed19b2741e56c8
John Munro <mrwizard@wizardstower.co.uk>
2023-10-28 14:01:23 -0400

    Add debug text

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # shake-light
 Shake Light
+
+todo:
+* service worker to allow it to be added to home page
+* better color
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+	const Max = 1000;
 	let battery = 0;
+	let x;
+	let y;
+	let z;
 
 	const ac = new Accelerometer({ frequency: 4 });
 	ac.addEventListener("reading", () => {
 		const total = ac.x + ac.y + ac.z;
 
-		battery += total;
+		battery = Math.Min(battery + total, Max);
 	});
 	ac.addEventListener("error", (event) => {
 		if (event.error.name === "NotAllowedError") {
@@ -17,11 +21,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	ac.start();
 
 	window.setInterval(function() {
-		const batteryAdjusted = Math.min(battery, 1000);
-		const brightness = (battery / 1000) * 255;
+		const brightness = (battery / Max) * 255;
 
 		document.body.style.backgroundColor = `rgb(${brightness}, ${brightness}, ${brightness})`;
 
 		battery = Math.max(battery--, 0);
+
+		document.getElementById("x").innerHTML = x;
+		document.getElementById("y").innerHTML = x;
+		document.getElementById("z").innerHTML = x;
+		document.getElementById("battery").innerHTML = x;
 	}, 4);
 });
